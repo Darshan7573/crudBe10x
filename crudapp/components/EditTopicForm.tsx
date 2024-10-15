3ad976a6bd24d7eaff0225cac50cd1c909c81f62
Darshan7573 <darshu7573@gmail.com>
2024-10-15 13:20:43 +0530

    updated to older one deployment issue

--- crudapp/components/EditTopicForm.tsx
+++ crudapp/components/EditTopicForm.tsx
@@ -23,7 +23,7 @@ export default function EditTopicForm({
     e.preventDefault();
 
     try {
-      const res = await fetch(`/api/topics/${id}`, {
+      const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
         method: "PUT",
         headers: {
           "Content-type": "application/json",
--- crudapp/libs/mongodb.ts
+++ crudapp/libs/mongodb.ts
@@ -1,23 +1,11 @@
 import mongoose from "mongoose";
 
-let isConnected = false; // Global variable to track the connection status
-
 const connectMongoDB = async (): Promise<void> => {
-  if (isConnected) {
-    console.log("Already connected to MongoDB.");
-    return;
-  }
-
   try {
-    const dbConnection = await mongoose.connect(
-      process.env.MONGODB_URI as string
-    );
-
-    isConnected = dbConnection.connections[0].readyState === 1;
+    await mongoose.connect(process.env.MONGODB_URI as string);
     console.log("Connected to MongoDB.");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
-    throw new Error("MongoDB connection failed");
   }
 };
 
