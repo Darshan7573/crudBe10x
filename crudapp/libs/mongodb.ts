392287d10598ea0ef75b1bbbd21ec7bb1aaf3743
Darshan7573 <darshu7573@gmail.com>
2024-10-15 13:13:10 +0530

    mongodb updated

--- crudapp/libs/mongodb.ts
+++ crudapp/libs/mongodb.ts
@@ -1,11 +1,23 @@
 import mongoose from "mongoose";
 
+let isConnected = false; // Global variable to track the connection status
+
 const connectMongoDB = async (): Promise<void> => {
+  if (isConnected) {
+    console.log("Already connected to MongoDB.");
+    return;
+  }
+
   try {
-    await mongoose.connect(process.env.MONGODB_URI as string);
+    const dbConnection = await mongoose.connect(
+      process.env.MONGODB_URI as string
+    );
+
+    isConnected = dbConnection.connections[0].readyState === 1;
     console.log("Connected to MongoDB.");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
+    throw new Error("MongoDB connection failed");
   }
 };
 
